706fe170753e6602f80d83c3f71a2b1280494901
Eladkay <eladkay@gmail.com>
2015-11-07 12:56:14 +0200

    Ok made a mod file, slow progress eh

--- src/main/java/com/sirhuntpredator/pixelplus/PixelPlus.java
+++ src/main/java/com/sirhuntpredator/pixelplus/PixelPlus.java
@@ -1,5 +1,21 @@
 package com.sirhuntpredator.pixelplus;
 
-public class PixelPlus {
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
+@Mod(modid = PixelPlus.MODID, version = PixelPlus.VERSION, name = PixelPlus.NAME)
+public class PixelPlus
+{
+    public static final String MODID = "PixelPlus";
+    public static final String VERSION = "1.0";
+    public static final String NAME = "PixelPlus";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
 }
